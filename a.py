9b324a1a57f306d7279ad947d9881dbfa7060168
Aysegul Kaya <aysegulky007@gmail.com>
2025-09-16 15:11:31 +0300

    iki deneme py dosyası

---
+++ a.py
@@ -0,0 +1,2 @@
+def deneme (x):
+    print(f"This is a function in module a with argument: {x}")
---
+++ main.py
@@ -0,0 +1,4 @@
+from a import deneme
+deneme(x="Hello from main.py")
+
+deneme(x="Hello from main.py")
